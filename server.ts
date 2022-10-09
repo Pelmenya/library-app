49c909da794abdfa084b8e4b8f25a706061f1b09
Dmitry Lyapin <lyapindm@yandex.ru>
2022-10-09 18:32:26 +0300

    Заготовка

--- server.ts
+++ server.ts
@@ -1,10 +1,10 @@
-import { app } from './build/app.js';
+import { app } from './src/app';
 import path from 'path';
 
 const hostname = '0.0.0.0'; // сервер запустим на всех интерфейсах
 
 const { PORT = 3000 } = process.env;
 
-app.listen(PORT, hostname, () => {
+app.listen(3000, hostname, () => {
     console.log(`listening on port ${PORT}! (in folder ${path.resolve()})`);
 });

0714636de851c3e3317185dff655237ec37fca93
Dmitry Lyapin <lyapindm@yandex.ru>
2022-10-10 11:07:17 +0300

    Сделал задание 2.1.1.

--- server.ts
+++ server.ts
@@ -1,10 +1,9 @@
 import { app } from './src/app';
-import path from 'path';
 
 const hostname = '0.0.0.0'; // сервер запустим на всех интерфейсах
 
 const { PORT = 3000 } = process.env;
 
 app.listen(3000, hostname, () => {
-    console.log(`listening on port ${PORT}! (in folder ${path.resolve()})`);
+    console.log(`listening on port ${PORT}! (in folder ${__dirname})`);
 });

20fce7e6b182e0589996c87fb34d4d416e4360a0
Dmitry Lyapin <lyapindm@yandex.ru>
2022-10-18 20:13:37 +0300

    Сделал страницу книги

--- src/controllers/view-controller.ts
+++ src/controllers/view-controller.ts
@@ -1,6 +1,18 @@
-import { libraryDB } from '../db/libraryDB';
 import { Request, Response } from 'express';
+import { libraryDB } from '../db/libraryDB';
+import { notFound404Text } from '../constants/constants';
 
-export const getIndex =  (req: Request, res: Response) => {
-    res.render('index', { libraryDB });    
+export const getIndex = (req: Request, res: Response) => {
+    res.render('pages/index', { libraryDB });
+};
+
+export const getView = (req: Request, res: Response) => {
+    const { id } = req.params;
+    if (id) {
+        const book = libraryDB.books.find(item => item.id === id);
+        res.render('pages/view', { book });
+    } else {
+        res.status(404);
+        res.json(notFound404Text);
+    }
 };
--- src/routers/view-router.ts
+++ src/routers/view-router.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
-import { getIndex } from '../controllers/view-controller';
-import { ViewRoutes } from './routes';
+import { getIndex, getView } from '../controllers/view-controller';
+import { ViewRoutes, BooksRoutes } from './routes';
 
 const { INDEX, VIEW, CREATE, UPDATE } = ViewRoutes;
-
+const { ID } = BooksRoutes;
 
 const viewRouter = Router();
 
 viewRouter.get(`${INDEX}`, getIndex);
+
+viewRouter.get(`${VIEW}${ID}`, getView);
 
 export { viewRouter };

98ba0a684497a7eda1cced1bb6c1b9af80bba2ed
Dmitry Lyapin <lyapindm@yandex.ru>
2022-12-17 13:41:32 +0300

    Сделал класс BooksController, включил режимы согласно тз, интерфейс IBook

--- src/controllers/books-controller.ts
+++ src/controllers/books-controller.ts
@@ -10,59 +10,29 @@ import { Books } from '../models/books';
 
 const { NOT_FOUND_404 } = ROUTES;
 
-const getBooks = (req: Request, res: Response) => {
-    const handler = async () => {
-        try {
-            const books = await Books.find();
-            res.status(200);
-            res.json(books);
-        } catch (e) {
-            res.status(500).json(e);
-        }
-    };
-    handler().catch(e => console.log(e));
-};
+export class BooksController {
 
-const getBook = (req: Request, res: Response) => {
-    const handler = async () => {
-        const { id } = req.params;
-        try {
-            const book = await Books.findById(id);
-            if (id && book) {
-                res.status(200);
-                res.json(book);
-            } else {
-                res.status(404);
-                res.redirect(NOT_FOUND_404);
-            }
-        } catch (e) {
-            res.status(500).json(e);
-        }
-    };
-    handler().catch(e => console.log(e));
-};
-
-const createBook = (req: Request, res: Response) => {
-    const handler = async () => {
-        if (req.body && req.files) {
-            const files = req.files as TBookFiles;
-            let fileBook = '';
-            let fileCover = '';
-            let fileName = '';
-            if (files.fileBook) {
-                fileBook = files.fileBook[0].filename;
-                fileName = files.fileBook[0].originalname;
-            }
-            if (files.fileCover) {
-                fileCover = files.fileCover[0].filename;
-            }
-
-            const newBook = new Books({ id: uuid(), ...req.body, fileCover, fileName, fileBook });
+    static getBooks = (req: Request, res: Response) => {
+        const handler = async () => {
             try {
-                if (newBook) {
-                    await newBook.save();
-                    res.status(201);
-                    res.redirect(`/view/${String(newBook._id)}`);
+                const books = await Books.find();
+                res.status(200);
+                res.json(books);
+            } catch (e) {
+                res.status(500).json(e);
+            }
+        };
+        handler().catch(e => console.log(e));
+    };
+
+    static getBook = (req: Request, res: Response) => {
+        const handler = async () => {
+            const { id } = req.params;
+            try {
+                const book = await Books.findById(id);
+                if (id && book) {
+                    res.status(200);
+                    res.json(book);
                 } else {
                     res.status(404);
                     res.redirect(NOT_FOUND_404);
@@ -70,118 +40,150 @@ const createBook = (req: Request, res: Response) => {
             } catch (e) {
                 res.status(500).json(e);
             }
-        }
+        };
+        handler().catch(e => console.log(e));
     };
-    handler().catch(e => console.log(e));
-};
 
-const updateBook = (req: Request, res: Response) => {
-    const handler = async () => {
-        const { id } = req.params;
-        try {
-            const book = await Books.findById(id);
-            const files = req.files as TBookFiles;
-
-            if (book && req.body) {
-                let fileBook = book.fileBook;
-                let fileCover = book.fileCover;
-                let fileName = book.fileName;
-
+    static createBook = (req: Request, res: Response) => {
+        const handler = async () => {
+            if (req.body && req.files) {
+                const files = req.files as TBookFiles;
+                let fileBook = '';
+                let fileCover = '';
+                let fileName = '';
                 if (files.fileBook) {
-                    if (fileBook) {
-                        deleteBookFile(fileBook);
-                    }
-
                     fileBook = files.fileBook[0].filename;
                     fileName = files.fileBook[0].originalname;
                 }
                 if (files.fileCover) {
-                    if (fileCover) {
-                        deleteBookFile(fileCover);
-                    }
                     fileCover = files.fileCover[0].filename;
                 }
 
-                const newBook: UpdateQuery<{
-                    title: string;
-                    description: string;
-                    authors: string;
-                    favorite: string;
-                    fileCover: string;
-                    fileName: string;
-                    fileBook: string;
-                }> | undefined = {
-                    ...req.body,
-                    fileBook,
-                    fileName,
-                    fileCover,
-                };
-
+                const newBook = new Books({ id: uuid(), ...req.body, fileCover, fileName, fileBook });
                 try {
-                    const upBook = await Books.findByIdAndUpdate({ _id: id }, newBook);
-                    res.status(200);
-                    res.redirect(`/view/${String(upBook?._id)}`);
+                    if (newBook) {
+                        await newBook.save();
+                        res.status(201);
+                        res.redirect(`/view/${String(newBook._id)}`);
+                    } else {
+                        res.status(404);
+                        res.redirect(NOT_FOUND_404);
+                    }
                 } catch (e) {
                     res.status(500).json(e);
                 }
-            } else {
-                res.status(404);
-                res.redirect(NOT_FOUND_404);
             }
-        } catch (e) {
-            res.status(500).json(e);
-        }
+        };
+        handler().catch(e => console.log(e));
     };
-    handler().catch(e => console.log(e));
-};
 
-const deleteBook = (req: Request, res: Response) => {
-    const handler = async () => {
-        const { id } = req.params;
-        try {
-            const book = await Books.findByIdAndRemove(id);
-            if (book && req.body) {
-                if (book.fileBook) {
-                    deleteBookFile(book.fileBook);
+    static updateBook = (req: Request, res: Response) => {
+        const handler = async () => {
+            const { id } = req.params;
+            try {
+                const book = await Books.findById(id);
+                const files = req.files as TBookFiles;
+
+                if (book && req.body) {
+                    let fileBook = book.fileBook;
+                    let fileCover = book.fileCover;
+                    let fileName = book.fileName;
+
+                    if (files.fileBook) {
+                        if (fileBook) {
+                            deleteBookFile(fileBook);
+                        }
+
+                        fileBook = files.fileBook[0].filename;
+                        fileName = files.fileBook[0].originalname;
+                    }
+                    if (files.fileCover) {
+                        if (fileCover) {
+                            deleteBookFile(fileCover);
+                        }
+                        fileCover = files.fileCover[0].filename;
+                    }
+
+                    const newBook: UpdateQuery<{
+                        title: string;
+                        description: string;
+                        authors: string;
+                        favorite: string;
+                        fileCover: string;
+                        fileName: string;
+                        fileBook: string;
+                    }> | undefined = {
+                        ...req.body,
+                        fileBook,
+                        fileName,
+                        fileCover,
+                    };
+
+                    try {
+                        const upBook = await Books.findByIdAndUpdate({ _id: id }, newBook);
+                        res.status(200);
+                        res.redirect(`/view/${String(upBook?._id)}`);
+                    } catch (e) {
+                        res.status(500).json(e);
+                    }
+                } else {
+                    res.status(404);
+                    res.redirect(NOT_FOUND_404);
                 }
-                if (book.fileCover) {
-                    deleteBookFile(book.fileCover);
+            } catch (e) {
+                res.status(500).json(e);
+            }
+        };
+        handler().catch(e => console.log(e));
+    };
+
+    static deleteBook = (req: Request, res: Response) => {
+        const handler = async () => {
+            const { id } = req.params;
+            try {
+                const book = await Books.findByIdAndRemove(id);
+                if (book && req.body) {
+                    if (book.fileBook) {
+                        deleteBookFile(book.fileBook);
+                    }
+                    if (book.fileCover) {
+                        deleteBookFile(book.fileCover);
+                    }
+
+                    res.status(200);
+                    res.redirect('/index');
+                } else {
+                    res.status(404);
+                    res.redirect(NOT_FOUND_404);
                 }
-
-                res.status(200);
-                res.redirect('/index');
-            } else {
-                res.status(404);
-                res.redirect(NOT_FOUND_404);
+            } catch (e) {
+                res.status(500).json(e);
             }
-        } catch (e) {
-            res.status(500).json(e);
-        }
-    };
-    handler().catch(e => console.log(e));
+        };
+        handler().catch(e => console.log(e));
 
-};
-
-const downLoadBook = (req: Request, res: Response) => {
-    const handler = async () => {
-        const { id } = req.params;
-        try {
-            const book = await Books.findById(id);
-
-            if (book?.fileBook) {
-                res.status(200);
-                const file = `${publicBooksFilesDir}/${book.fileBook}`;
-                res.download(file, book.fileName);
-            } else {
-                res.status(404);
-                res.redirect(NOT_FOUND_404);
-            }
-        } catch (e) {
-            res.status(500).json(e);
-        }
     };
 
-    handler().catch((e) => console.log(e));
-};
+    static downLoadBook = (req: Request, res: Response) => {
+        const handler = async () => {
+            const { id } = req.params;
+            try {
+                const book = await Books.findById(id);
 
-export { getBooks, getBook, createBook, updateBook, deleteBook, downLoadBook };
+                if (book?.fileBook) {
+                    res.status(200);
+                    const file = `${publicBooksFilesDir}/${book.fileBook}`;
+                    res.download(file, book.fileName);
+                } else {
+                    res.status(404);
+                    res.redirect(NOT_FOUND_404);
+                }
+            } catch (e) {
+                res.status(500).json(e);
+            }
+        };
+
+        handler().catch((e) => console.log(e));
+    };
+
+}
--- src/routers/api-router/books-router/books-router.ts
+++ src/routers/api-router/books-router/books-router.ts
@@ -1,12 +1,5 @@
-import {
-    createBook,
-    deleteBook,
-    downLoadBook,
-    getBook,
-    getBooks,
-    updateBook,
-} from '../../../controllers/books-controller';
 
+import { BooksController } from '../../../controllers/books-controller';
 import { Router } from 'express';
 import { ROUTES } from '../../../utils/constants/routes';
 
@@ -14,16 +7,16 @@ const { BOOKS, ID, DOWNLOAD } = ROUTES;
 
 const booksRouter = Router();
 
-booksRouter.get(BOOKS, getBooks);
+booksRouter.get(BOOKS, BooksController.getBooks);
 
-booksRouter.get(`${BOOKS}${ID}`, getBook);
+booksRouter.get(`${BOOKS}${ID}`, BooksController.getBook);
 
-booksRouter.post(BOOKS, createBook);
+booksRouter.post(BOOKS, BooksController.createBook);
 
-booksRouter.put(`${BOOKS}${ID}`, updateBook);
+booksRouter.put(`${BOOKS}${ID}`, BooksController.updateBook);
 
-booksRouter.delete(`${BOOKS}${ID}`, deleteBook);
+booksRouter.delete(`${BOOKS}${ID}`, BooksController.deleteBook);
 
-booksRouter.get(`${BOOKS}${ID}${DOWNLOAD}`, downLoadBook);
+booksRouter.get(`${BOOKS}${ID}${DOWNLOAD}`, BooksController.downLoadBook);
 
 export { booksRouter };
